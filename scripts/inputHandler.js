84ffb9669077e02770ca70ac63f08aa4469ef6d5
TennielTakenaka <tenniel.lynn@gmail.com>
2024-02-03 12:37:44 -0800

    First toast iteration, got toast to slide across screen.

---
+++ scripts/inputHandler.js
@@ -0,0 +1,17 @@
+// addEventListener("keydown", function(event) {
+//     if (event.code == 'KeyD'){
+//         console.log(event.code)
+//         vx=5;
+//     }
+// })
+
+// addEventListener("keyup", function(event) {
+//     if (event.code == 'KeyD')
+//         vx=0;
+// })
+
+// TODO: Need to fix the event listener so that the box
+// continuously moves when you click on something
+toaster.addEventListener("click", function(event){
+    vy=5;
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,31 @@
-let makeToastBtn = document.querySelector("button"); // grabs by selector aka button or h1 or whatever
+const specialToast = document.getElementById("specialToast");
+const toaster = document.getElementById("toaster");
 
-function showToastPicture()
-{
-    const specialToast = document.getElementById("special-toast");
-    specialToast.style.display = "block";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let x=0;
+let y=0;
+let vx = 0;
+let vy = 0;
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  x += vx;
+  y += vy;
+  ctx.drawImage(specialToast, x, y);
+  requestAnimationFrame(update);
 }
 
-makeToastBtn.onclick = () => {
-    showToastPicture();
-}
+update();
 
+// myImage.addEventListener("click", changePicture);
+
+// function changePicture() {
+//     const mySrc = myImage.getAttribute("src");
+//     if (mySrc === "images/toaster.jpg") {
+//         myImage.setAttribute("src", "images/cute-toaster.png");
+//       } else {
+//         myImage.setAttribute("src", "images/toaster.jpg");
+//       }
+// }
